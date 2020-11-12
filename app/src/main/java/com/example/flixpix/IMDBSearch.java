018b9b4a64bc9d94926183aebdf77b1dc4a92ff7
Grant Von Haden <grantvonhaden@gmail.com>
2020-11-11 21:57:16 -0600

    work in progress json parsing

--- app/src/main/java/com/example/flixpix/IMDBSearch.java
+++ app/src/main/java/com/example/flixpix/IMDBSearch.java
@@ -3,8 +3,13 @@ package com.example.flixpix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,18 +28,78 @@ import okhttp3.Response;
 
 public class IMDBSearch extends AppCompatActivity {
 
+    ImageView thumbnail;
+    TextView movie_title;
+
+    String searched;
     String title;
+    String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.imdb_search);
 
+        thumbnail = (ImageView)findViewById(R.id.thumbnail);
+        movie_title = (TextView)findViewById(R.id.movie_title);
+
         Intent i = getIntent();
 
-        title = i.getStringExtra("title");
-        title.replaceAll(" ", "%20");
+        searched = i.getStringExtra("title");
+        searched.replaceAll(" ", "%20");
 
-        new MyTask().execute();
+        //sendGetRequest();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try  {
+                    OkHttpClient client = new OkHttpClient();
+
+                    Request request = new Request.Builder()
+                            .url("https://rapidapi.p.rapidapi.com/title/find?q=" + searched)
+                            .get()
+                            .addHeader("x-rapidapi-key", "7c57c58d8cmshc44eb6b914339e8p15232djsn6acde32e5206")
+                            .addHeader("x-rapidapi-host", "imdb8.p.rapidapi.com")
+                            .build();
+
+                    try
+                    {
+                        Response response = client.newCall(request).execute();
+                        String responseBody = response.body().string();
+                        System.out.println(responseBody);
+                        JSONObject object = new JSONObject(responseBody);
+
+                        JSONArray results = object.getJSONArray("results");
+
+                        for(int x = 0; x < results.length(); x++)
+                        {
+                            JSONObject result = results.getJSONObject(x);
+                            String titleType = result.getString("titleType");
+                            if(titleType.equals("movie") && !titleType.equals("") && !titleType.equals(null))
+                            {
+                                title = result.getString("title");
+                                JSONObject url_data = result.getJSONObject("image");
+                                url = url_data.getString("url");
+
+                                movie_title.setText(title);
+                                Picasso.with(IMDBSearch.this).load(url).into(thumbnail);
+
+                                break;
+                            }
+                        }
+                        System.out.println("");
+                    }
+                    catch (IOException | JSONException e)
+                    {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
     }
 
     private class MyTask extends AsyncTask<Void, Void, Void> {
@@ -43,7 +108,7 @@ public class IMDBSearch extends AppCompatActivity {
             OkHttpClient client = new OkHttpClient();
 
             Request request = new Request.Builder()
-                    .url("https://rapidapi.p.rapidapi.com/title/find?q=" + title)
+                    .url("https://rapidapi.p.rapidapi.com/title/find?q=" + searched)
                     .get()
                     .addHeader("x-rapidapi-key", "7c57c58d8cmshc44eb6b914339e8p15232djsn6acde32e5206")
                     .addHeader("x-rapidapi-host", "imdb8.p.rapidapi.com")
@@ -56,13 +121,20 @@ public class IMDBSearch extends AppCompatActivity {
                 System.out.println(responseBody);
                 JSONObject object = new JSONObject(responseBody);
 
-                //JSONArray id = object.getJSONArray("titleType");
-                //System.out.println(id);
-
-                //Iterator<String> keys = object.keys();
-                //System.out.println("");
-
                 JSONArray results = object.getJSONArray("results");
+
+                for(int x = 0; x < results.length(); x++)
+                {
+                    JSONObject result = results.getJSONObject(x);
+                    String titleType = result.getString("titleType");
+                    if(titleType.equals("movie") && !titleType.equals("") && !titleType.equals(null))
+                    {
+                        title = result.getString("title");
+                        JSONObject url_data = result.getJSONObject("image");
+                        url = url_data.getString("url");
+                        break;
+                    }
+                }
                 System.out.println("");
             }
             catch (IOException | JSONException e)
